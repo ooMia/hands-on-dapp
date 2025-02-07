dfc209859163b0f41b766b65aaa50d629a4888bc
Hyeon-hak Kim <hyeonhak.kim.dev@gmail.com>
2025-02-07 20:42:12 +0900

    ✨ Feat : build contract

---
+++ src/sol/hello.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.28;
+
+contract HelloWorld {
+    function getName() public pure returns (string memory) {
+        return "World";
+    }
+}
