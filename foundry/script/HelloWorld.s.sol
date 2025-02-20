24f04d662ef76cd918182aea618779def0add85b
Hyeon-hak Kim <hyeonhak.kim.dev@gmail.com>
2025-02-20 23:05:30 +0900

    🔄 Refactor : simplify

--- foundry/script/HelloWorld.s.sol
+++ foundry/script/HelloWorld.s.sol
@@ -5,22 +5,15 @@ import {Script, console} from "forge-std/Script.sol";
 import {HelloWorld} from "src/HelloWorld.sol";
 
 contract CounterScript is Script {
-    uint256 secret;
-    address admin;
     HelloWorld public world;
 
-    function setUp() public {
-        secret = vm.envUint("PRIVATE_KEY"); // .env: PRIVATE_KEY=0x1234...
-        admin = vm.rememberKey(secret);
-        require(secret != 0 && admin != address(0), "check .env");
-    }
-
     function run() public {
-        vm.startBroadcast();
+        vm.startBroadcast(vm.envUint("PRIVATE_KEY"));
 
         world = new HelloWorld();
         world.setName("World");
         require(isStringEqual(world.getName(), "World"), "setName failed");
+        console.log(msg.sender);
 
         vm.stopBroadcast();
     }
