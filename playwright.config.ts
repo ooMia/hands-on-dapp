d26ab507de351c7dcc1d8837baea7d1ee70ac31e
Hyeon-hak Kim <hyeonhak.kim.dev@gmail.com>
2025-02-26 05:20:06 +0900

    🛠️ Chore : pnpm test
    - playwright config
    - prettier

--- playwright.config.ts
+++ playwright.config.ts
@@ -12,7 +12,8 @@ import { defineConfig, devices } from "@playwright/test";
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-  testDir: "./src",
+  outputDir: "./out/e2e",
+  testDir: "./__tests__/e2e",
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
@@ -22,11 +23,11 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: "html",
+  reporter: [["list"], ["html", { outputFolder: "out/e2e" }]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: "http://127.0.0.1:3000",
+    baseURL: "http://127.0.0.1:3000/",
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
@@ -72,8 +73,8 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: "pnpm start",
-    url: "http://127.0.0.1:3000",
+    command: "pnpm dev",
+    url: "http://127.0.0.1:3000/",
     reuseExistingServer: !process.env.CI,
   },
 });
--- src/app/page.spec.ts
+++
@@ -1,9 +0,0 @@
-import { expect, test } from "@playwright/test";
-
-test.beforeEach(async ({ page }) => {
-  await page.goto("/");
-});
-
-test("title", async ({ page }) => {
-  await expect(page.locator("#title")).toContainText(/fluent react/i);
-});
