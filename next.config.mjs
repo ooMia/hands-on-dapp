ae3bbd3d4c298eb345d4bf6c454da9ab11ed898b
Hyeon-hak Kim <hyeonhak.kim.dev@gmail.com>
2025-03-04 13:25:26 +0900

    🚧 WIP : try changing export style

--- next.config.mjs
+++ next.config.mjs
@@ -1,41 +1,43 @@
 import "dotenv/config";
 import { PHASE_DEVELOPMENT_SERVER } from "next/constants.js";
 
-let nextConfig = {};
-
-// @see https://github.com/actions/configure-pages/blob/v5/src/set-pages-config.js
 /**
  * @type {import('next').NextConfig}
  */
-const ghPagesConfig = {
-  ...nextConfig,
-  output: "export",
+const ci = {
+  // @see https://github.com/actions/configure-pages/blob/v5/src/set-pages-config.js
+  // output: "export",
   // basePath: `/${JSON.parse(readFileSync("package.json", "utf-8")).name}`,
   // pageExtensions: ["jsx", "js", "ts", "tsx"],
 };
 
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+  compiler: {
+    reactRemoveProperties: true,
+  },
+};
+
 export default (phase, { defaultConfig }) => {
-  /**
-   * @type {import('next').NextConfig}
-   */
-  const config = {
-    ...defaultConfig,
-    env: {
-      NEXT_PHASE: phase,
-    },
-    compiler: {
-      reactRemoveProperties: true,
-    },
-  };
   if (phase !== PHASE_DEVELOPMENT_SERVER) {
-    Object.assign(config.compiler, {
+    Object.assign(nextConfig.compiler, {
       removeConsole: {
         exclude: ["error"],
       },
     });
   }
+
   if (process.env.CI === "true") {
-    Object.assign(config, ghPagesConfig);
+    Object.assign(nextConfig, ci);
   }
-  return config;
+
+  return {
+    env: {
+      NEXT_PHASE: phase,
+    },
+    ...defaultConfig,
+    ...nextConfig,
+  };
 };
