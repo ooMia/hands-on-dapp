08002036c9011770cdeae3d804e3ee4bde850e6a
Hyeon-hak Kim <hyeonhak.kim.dev@gmail.com>
2025-03-04 13:30:35 +0900

    🚧 WIP : try changing export style (2)

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,3 @@
-import "dotenv/config";
-import { PHASE_DEVELOPMENT_SERVER } from "next/constants.js";
-
-/**
- * @type {import('next').NextConfig}
- */
-const ci = {
-  // @see https://github.com/actions/configure-pages/blob/v5/src/set-pages-config.js
-  // output: "export",
-  // basePath: `/${JSON.parse(readFileSync("package.json", "utf-8")).name}`,
-  // pageExtensions: ["jsx", "js", "ts", "tsx"],
-};
-
 /**
  * @type {import('next').NextConfig}
  */
@@ -20,24 +7,4 @@ const nextConfig = {
   },
 };
 
-export default (phase, { defaultConfig }) => {
-  if (phase !== PHASE_DEVELOPMENT_SERVER) {
-    Object.assign(nextConfig.compiler, {
-      removeConsole: {
-        exclude: ["error"],
-      },
-    });
-  }
-
-  if (process.env.CI === "true") {
-    Object.assign(nextConfig, ci);
-  }
-
-  return {
-    env: {
-      NEXT_PHASE: phase,
-    },
-    ...defaultConfig,
-    ...nextConfig,
-  };
-};
+export default nextConfig;
