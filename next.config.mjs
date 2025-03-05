be64d73d31bb6a0d40b87015a1d473deb8bbfb59
Hyeon-hak Kim <hyeonhak.kim.dev@gmail.com>
2025-03-05 13:01:35 +0900

    🛠️ Chore : modify export style

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,38 @@
 /**
+ * This file should follow a few conventions to work with GitHub actions/configure-pages:
+ * 1. Only js, cjs, and mjs extensions are supported.
+ *    See: https://github.com/actions/configure-pages/blob/main/src/set-pages-config.js#L7
+ * 2. Only 10 cases of configuration initialization styles are supported.
+ *    See: https://github.com/actions/configure-pages/blob/main/src/config-parser.js#L71
+ */
+
+import "dotenv/config";
+
+/**
  * @type {import('next').NextConfig}
  */
-const nextConfig = {
+const baseConfig = {
   compiler: {
     reactRemoveProperties: true,
   },
+  experimental: {},
 };
 
-export default nextConfig;
+export default injectOptional(baseConfig);
+
+function injectOptional(
+  /**
+   * @type {import('next').NextConfig}
+   */
+  config,
+) {
+  if (process.env.NODE_ENV === "development") {
+    config.compiler.removeConsole = false;
+    config.experimental.allowDevelopmentBuild = true;
+
+    if (process.env.CI === "true") {
+      config.output = "export";
+    }
+  }
+  return config;
+}
